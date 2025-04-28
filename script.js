ec56059ee58f79ce40f10e9569aa4944c26d6e4b
wojtek-smyczek <wojciech.smyczek@student.uj.edu.pl>
2025-04-28 18:20:53 +0200

    Hover over pixel effect implemented in JS

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const outer_container = document.createElement('div');
 outer_container.classList.add('outer_container');
 document.body.appendChild(outer_container);
@@ -11,8 +9,23 @@ for (let j = 0; j < 15; j++) {
 
     for (let i = 0; i < 15; i++) {
         const div = document.createElement('div');
+        div.classList.add('pixel');
         container.appendChild(div);
     }
     outer_container.appendChild(container);
 }
+
+
+// --------hover over the pixel------------
+
+let pixels = document.querySelectorAll('.pixel');
+
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = 'black';
+    });
+    pixel.addEventListener('mouseout', () => {
+        pixel.style.backgroundColor = '';
+    })
+})
 
