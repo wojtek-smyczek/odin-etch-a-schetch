7ce66063caa1228b827b359eff5ea52deb652d9b
wojtek-smyczek <wojciech.smyczek@student.uj.edu.pl>
2025-04-28 19:03:50 +0200

    Something doesnt work with reseting the canvas

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-const outer_container = document.createElement('div');
-outer_container.classList.add('outer_container');
-document.body.appendChild(outer_container);
+let outer_container;
+
+function clearTheCanvas() {
+    outer_container.remove();
+}
+function drawTheCanvas(size) {
+
+    outer_container = document.createElement('div');
+    outer_container.classList.add('outer_container');
+    document.body.appendChild(outer_container);
 
 
-for (let j = 0; j < 15; j++) {
-    const container = document.createElement('div');
-    container.classList.add('container');
+    for (let j = 0; j < size; j++) {
+        const container = document.createElement('div');
+        container.classList.add('container');
 
-    for (let i = 0; i < 15; i++) {
-        const div = document.createElement('div');
-        div.classList.add('pixel');
-        container.appendChild(div);
+        for (let i = 0; i < size; i++) {
+            const div = document.createElement('div');
+            div.classList.add('pixel');
+            container.appendChild(div);
+        }
+        outer_container.appendChild(container);
     }
-    outer_container.appendChild(container);
+    addPixelHoverEffect();
 }
 
 
 // --------hover over the pixel------------
+function addPixelHoverEffect() {
 
-let pixels = document.querySelectorAll('.pixel');
+    let pixels = document.querySelectorAll('.pixel');
 
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', () => {
-        pixel.style.backgroundColor = 'black';
-    });
-    // pixel.addEventListener('mouseout', () => {
-    //     pixel.style.backgroundColor = '';
-    // })
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = 'black';
+        });
+        // pixel.addEventListener('mouseout', () => {
+        //     pixel.style.backgroundColor = '';
+        // })
+    })
+}
+
+// ----------button enter number of boxes---------------
+
+let button = document.querySelector('.button1');
+button.addEventListener('click', () => {
+
+    let personInput = prompt("Enter the number of desired squares!");
+
+    if (personInput == null || personInput == "" || personInput > 100 || personInput < 0) {
+        text = "Invalid";
+    } else {
+        clearTheCanvas();
+        drawTheCanvas(Number(personInput));
+    }
+
+
 })
 
